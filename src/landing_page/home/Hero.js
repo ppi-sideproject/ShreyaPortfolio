9cdc1a1db52af1e352303f83fe6dc879a2306b50
Shreya Raut <shreyaraut061@gmail.com>
2025-12-01 12:32:27 +0530

    feat(hero.js): small change

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero <i class="fa fa-bath" aria-hidden="true"></i></h1>
+        <h1>Hero! <i class="fa fa-bath" aria-hidden="true"></i></h1>
      );
 }
 
