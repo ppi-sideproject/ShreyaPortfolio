d7428ea3dcf37c77d2a780a7b7db134b081dc90a
Shreya Raut <shreyaraut061@gmail.com>
2025-12-04 11:15:07 +0530

    Changes in index.js file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
 import HomePage from './landing_page/home/HomePage';
 import AboutPage from './landing_page/about/AboutPage';
 import ProjectPage from './landing_page/project/ProjectPage';
-import ContactPage from './landing_page/contact/ContactPage';       
-
+import ContactPage from './landing_page/contact/ContactPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/about' element={<AboutPage/>}/>
-    <Route path='/project' element={<ProjectPage/>}/>
-    <Route path='contact' element={<ContactPage/>}/>
-  </Routes> 
-  <Footer/>
-  </BrowserRouter>
+  <HashRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/project' element={<ProjectPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+    </Routes>
+    <Footer />
+  </HashRouter>
 );
-
-
