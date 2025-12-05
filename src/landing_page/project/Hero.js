1d905cf4becb5516cc3a33ae3f992b71d88886a4
Shreya Raut <shreyaraut061@gmail.com>
2025-12-05 13:32:27 +0530

    Added files

--- src/landing_page/project/Hero.js
+++ src/landing_page/project/Hero.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Project</h1>
-     );
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/ShreyaPortfolio/media/images/Project.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        minHeight: "92vh",
+        display: "flex",
+        alignItems: "center",
+        color: "white",
+      }}
+    >
+      <div className="container text-white py-5">
+        <div className="row">
+          <div className="col-12 col-md-10 col-lg-8">
+            <h1 className="mb-2">Projects</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
